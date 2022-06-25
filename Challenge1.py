f15050218f93c43842de5373e2bc41f631eb84c4
lukoscodes <plukosevic@gmail.com>
2022-06-25 08:25:11 +0100

    Python Training

---
+++ Challenge1.py
@@ -0,0 +1,10 @@
+food = input("What's my favourite food?")
+
+if food == "sweets" or "Sweets":
+    print("Yep! So amazing!")
+
+else:
+    print("Yuck! That's not it!")
+
+    print("Thanks for playing!")
+
